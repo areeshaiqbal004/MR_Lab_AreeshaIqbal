6c67f0854678e0677b5f48c7abdf25bee7f38b30
Areesha Iqbal <areesha.iqbal004@gmail.com>
2026-03-22 23:54:41 +0500

    Moved turtle_package_circular directly under src

---
+++ turtle_package_circular/setup.py
@@ -0,0 +1,26 @@
+from setuptools import setup
+
+package_name = 'turtle_package_circular'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=[package_name],
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='areesha',
+    maintainer_email='areesha.iqbal004@gmail.com',
+    description='Week 3 turtle circular package',
+    license='TODO: License declaration',
+    tests_require=['pytest'],
+    entry_points={
+        'console_scripts': [
+            'circle_turtle = turtle_package_circular.circle_turtle:main',
+        ],
+    },
+)
