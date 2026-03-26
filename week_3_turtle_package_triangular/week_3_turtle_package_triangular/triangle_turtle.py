3a41e50cfc2f0195652b3269d3020db89e94ced4
Areesha Iqbal <areesha.iqbal004@gmail.com>
2026-03-26 11:00:45 +0500

    Updated triangle node with loop using timer

--- week_3_turtle_package_triangular/week_3_turtle_package_triangular/triangle_turtle.py
+++ week_3_turtle_package_triangular/week_3_turtle_package_triangular/triangle_turtle.py
@@ -1,36 +1,50 @@
-import time
-
 import rclpy
-from geometry_msgs.msg import Twist
 from rclpy.node import Node
+from geometry_msgs.msg import Twist
 
 
 class TrianglePublisher(Node):
     def __init__(self):
         super().__init__('triangle_publisher')
+
         self.publisher_ = self.create_publisher(Twist, '/turtle1/cmd_vel', 10)
-        self.move_triangle()
+
+        self.step = 0
+        self.counter = 0
+
+        # Timer (loop)
+        self.timer = self.create_timer(0.5, self.move_triangle)
 
     def move_triangle(self):
-        for _ in range(3):
-            msg = Twist()
+        msg = Twist()
+
+        # Move forward
+        if self.step == 0:
             msg.linear.x = 2.0
             msg.angular.z = 0.0
-            self.publisher_.publish(msg)
-            time.sleep(2)
+            self.counter += 1
 
+            if self.counter >= 4:
+                self.step = 1
+                self.counter = 0
+
+        # Turn
+        elif self.step == 1:
             msg.linear.x = 0.0
             msg.angular.z = 2.09
-            self.publisher_.publish(msg)
-            time.sleep(1)
+            self.counter += 1
 
-        stop_msg = Twist()
-        self.publisher_.publish(stop_msg)
+            if self.counter >= 2:
+                self.step = 0
+                self.counter = 0
+
+        self.publisher_.publish(msg)
 
 
 def main(args=None):
     rclpy.init(args=args)
     node = TrianglePublisher()
+    rclpy.spin(node)
     node.destroy_node()
     rclpy.shutdown()
 
