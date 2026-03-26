37141da053573094f28c6f3fbb762e6e0eed30c8
Areesha Iqbal <areesha.iqbal004@gmail.com>
2026-03-26 10:21:26 +0500

    Added triangle turtle node

---
+++ triangle_task/setup.py
@@ -0,0 +1,30 @@
+from setuptools import find_packages, setup
+
+package_name = 'triangle_task'
+
+setup(
+    name=package_name,
+    version='0.0.0',
+    packages=find_packages(exclude=['test']),
+    data_files=[
+        ('share/ament_index/resource_index/packages',
+            ['resource/' + package_name]),
+        ('share/' + package_name, ['package.xml']),
+    ],
+    install_requires=['setuptools'],
+    zip_safe=True,
+    maintainer='areesha',
+    maintainer_email='areesha.iqbal004@gmail.com',
+    description='TODO: Package description',
+    license='TODO: License declaration',
+    extras_require={
+        'test': [
+            'pytest',
+        ],
+    },
+    entry_points={
+    'console_scripts': [
+        'triangle_turtle = triangle_task.triangle_turtle:main',
+    ],
+},
+)
